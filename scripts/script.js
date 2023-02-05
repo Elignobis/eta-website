278f14b982e9f13a24ef2fab89946f17ba3b4d69
elignobis <rwayisibongile@gmail.com>
2023-02-05 17:42:05 +0200

    It did

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,13 @@ window.onscroll = function() {
       secondaryNav.style.display = "none";
     }
   };
+
+  window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
